e8ef7e8f48e30da8ccef8647d7e009fa42dee937
Chen <chen_util@qq.com>
2025-05-03 16:21:17 +0800

    插件接口鉴权

--- view/add_metas.ts
+++ view/add_metas.ts
@@ -10,7 +10,8 @@ export class addMetas extends Modal {
 	constructor(app: App, type: string, fun: () => Promise<void>) {
 		super(app);
 		this.type = type; // 0: tag, 1: category
-		this.type_name = type === "tag" ? i18n.t("field.tag") : i18n.t("field.category");
+		this.type_name =
+			type === "tag" ? i18n.t("field.tag") : i18n.t("field.category");
 		this.fun = fun;
 	}
 
@@ -24,7 +25,9 @@ export class addMetas extends Modal {
 			attr: {
 				type: "text",
 				placeholder:
-					i18n.t("common.input") + `${this.type_name} ` + i18n.t("common.enter"),
+					i18n.t("common.input") +
+					`${this.type_name} ` +
+					i18n.t("common.enter"),
 				style: "width: 100%; margin-top: 15px; margin-bottom: 15px; border: 1px solid var(--background-modifier-border); border-radius: 4px; color: var(--text-normal);",
 			},
 		});
@@ -41,18 +44,28 @@ export class addMetas extends Modal {
 				}
 				this.notice = new Notice(i18n.t("sync.adding") + "...");
 				try {
+					// 获取csrfToken
+					const token = await HttpUtils.get(
+						"/getCsrfToken?key=" + this.name
+					);
 					const data = {
 						name: this.name,
 						type: this.type,
+						token: token.data.csrfToken
 					};
+
 					const response = await HttpUtils.post("/addMetas", data);
 					if (response.status === "success") {
-						new Notice(`${this.type_name}` + i18n.t("sync.addSuccess"));
+						new Notice(
+							`${this.type_name}` + i18n.t("sync.addSuccess")
+						);
 						await this.fun();
 						this.close();
 					}
 				} catch (error) {
-					new Notice(`${this.type_name} ` + i18n.t("error.requestFailed"));
+					new Notice(
+						`${this.type_name} ` + i18n.t("error.requestFailed")
+					);
 					console.error("获取失败:", error);
 				}
 			}
--- view/push_modal.ts
+++ view/push_modal.ts
@@ -89,6 +89,9 @@ export class PushModal extends Modal {
 			}
 			this.notice = new Notice(`${i18n.t("sync.publish")}...`);
 			try {
+				// 获取csrfToken
+				const token = await HttpUtils.get("/getCsrfToken?key="+this.title);
+
 				const mid = [...this.selectedCategories, ...this.selectedTags];
 				const data = {
 					title: this.title,
@@ -96,6 +99,7 @@ export class PushModal extends Modal {
 					authorId: getSettings().User?.uid,
 					mid: mid.join(","),
 					slug: "obsidian-" + this.baseFileName,
+					token: token.data.csrfToken,
 				};
 				console.log(data);
 				const response = await HttpUtils.post("/postArticle", data);
